b683d18f413e47f0a729a22eb56b9b3218c2967b
Lunga Baliwe <lunga001@gmail.com>
2018-08-13 12:20:39 +0200

    Overriding COA

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ version = '1.0.0'
 setup(
     name='senaite.qlab',
     version=version,
-    description="SENAITE ADD-ON",
+    description="SENAITE QUANTUM LAB",
     # long_description=open("docs/About.rst").read() +
     #                 "\n\n" +
     #                 open("senaite/photoimpoter/docs/API.rst").read() +
